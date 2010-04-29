b46059efdee994124838ef7cecd958f23c61b5c4
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2010-04-29 14:53:03 +0900

    tag cloud changed to be good

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,9 +3,9 @@ class TopController < ApplicationController
     @goals = Goal.find(:all, :conditions=>{:is_deleted => false})
 
     # http://kjirou.sakura.ne.jp/mt/2007/09/post_57.html
-    weights = @goals.collect {|g| (g.term - g.created_at)}
-    coefficient = 24 / ( Math::sqrt(weights.max) - Math::sqrt(weights.min) )
-    @levels = weights.collect {|w| 24 - ( Math::sqrt(w) - Math::sqrt(weights.min) ) * coefficient }
+    weights = @goals.collect {|g| (g.term - Time.now)}
+    coefficient = 16 / Math::sqrt(weights.min)
+    @levels = weights.collect {|w| 16 - ( Math::sqrt(w) - Math::sqrt(DateTime.now) ) * coefficient }
   end
 
 end
