db1bb34faafd8c232e34cc9f0f3655c0b2709b47
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2010-04-29 12:57:09 +0900

    added "remove button".

--- app/controllers/editor_controller.rb
+++ app/controllers/editor_controller.rb
@@ -3,7 +3,7 @@ class EditorController < ApplicationController
   def add
     goal = Goal.create(
       :description=>params[:description],
-      :term=> Time.now + params[:term].to_i * 24*60*60
+      :term=> Time.now + dayToTime(params[:term].to_i)
     )
 
     goal.new_record?
@@ -12,12 +12,17 @@ class EditorController < ApplicationController
   def remove
     goal = Goal.find(params[:id])
     goal.is_deleted = true
+    goal.save
   end
 
   def edit
     goal = Goal.find(params[:id])
     goal.description = params[:description]
-    goal.term = Time.now + params[:term].to_i * 24*60*60
+    goal.term = Time.now + dayToTime(params[:term].to_i)
   end
 
+end
+
+def dayToTime(day)
+  day * 24*60*60
 end
--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,6 +1,6 @@
 class TopController < ApplicationController
   def index
-    @goals = Goal.find(:all)
+    @goals = Goal.find(:all, :conditions=>{:is_deleted => false})
   end
 
 end
