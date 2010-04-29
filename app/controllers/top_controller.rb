cf2d16323342408afd02fdbd91ce1dd48c646f2b
Toshiyuki Hirooka <toshi.hirooka@gmail.com>
2010-04-29 21:08:33 +0900

    page doesnt work on firebug not installed. fixed it.

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,10 +2,12 @@ class TopController < ApplicationController
   def index
     @goals = Goal.find(:all, :conditions=>{:is_deleted => false})
 
-    # http://kjirou.sakura.ne.jp/mt/2007/09/post_57.html
-    weights = @goals.collect {|g| (g.term - Time.now)}
-    coefficient = 16 / Math::sqrt(weights.min)
-    @levels = weights.collect {|w| 16 - ( Math::sqrt(w) - Math::sqrt(DateTime.now) ) * coefficient }
+    if @goals.size != 0
+      # http://kjirou.sakura.ne.jp/mt/2007/09/post_57.html
+      weights = @goals.collect {|g| (g.term - DateTime.now)}
+      coefficient = 36 / Math::sqrt(weights.max)
+      @levels = weights.collect {|w| 36 - Math::sqrt(w) * coefficient }
+    end
   end
 
 end
